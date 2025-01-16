7f96e867f7d881c0f94c5c7a3a53881d9629c605
M.Emad Rasooli <rasooliemad2004@gmail.com>
2025-01-16 14:59:11 +0430

    15-Refactor-Environment-Variables

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSession, signOut } from 'next-auth/react';
+import PostComponent from '@/components/PostComponent';
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { HiHome } from 'react-icons/hi2';
+import { IoPerson } from 'react-icons/io5';
+import PostForm from '@/components/PostForm';
+import { useQuery } from '@tanstack/react-query';
+
+interface Author {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface Category {
+  id: string;
+  name: string;
+}
+
+interface PostWithRelations {
+  id: string;
+  title: string;
+  content?: string;
+  author: Author;
+  category: Category;
+  createdAt: Date;
+}
+
+const Dashboard: React.FC = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState<PostWithRelations[]>([]);
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`)
+      if (!response.ok) throw new Error('Failed to fetch Posts');
+      return await response.json();
+    }
+  })
+
+  useEffect(() => {
+    if (data) {
+      setPosts(data);
+    }
+  }, [data])
+
+  const handleLogout = async () => {
+    signOut({ callbackUrl: "/" });
+  };
+
+  if (error) return <div className='h-screen flex flex-col justify-center items-center'>Something Went Wrong!</div>
+
+  return (
+    <div className="text-white max-w-3xl mx-auto p-6 flex flex-col space-y-6">
+      <h1 className="text-3xl font-medium">Dashboard</h1>
+      <Button onClick={() => router.push("/")} variant={"link"} className="text-gray-300 w-fit"><HiHome />Home</Button>
+      <div className="bg-white text-black p-4 flex flex-row items-center space-x-4 relative rounded-xl">
+          <div>
+            <IoPerson size={40}/>
+          </div>
+          <div>
+            <p className="text-lg font-semibold flex items-center gap-2">{session?.user?.name} <span className="text-xs text-white bg-green-700 px-2 rounded-full">{session?.user?.role}</span></p>
+            <p className="text-gray-600 text-medium">{session?.user?.email}</p>
+          </div>
+          <Button onClick={handleLogout} variant={"destructive"} className="absolute right-4">Logout</Button>
+      </div>
+          <PostForm />
+          <h2 className='text-2xl font-medium'>Posts</h2>
+          {isLoading ? (
+        <p className="text-gray-500 text-center">Loading...</p>
+      ) : (
+        <div className="flex flex-col justify-center space-y-6 max-w-3xl">
+          {posts.map((post) => (
+            <PostComponent
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content ?? ''}
+              author={post.author}
+              category={post.category}
+              createdAt={post.createdAt}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
